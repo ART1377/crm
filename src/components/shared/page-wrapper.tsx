65903b07ec6b8c1bd74458758bd34b2f0900d595
don <don@don.com>
2026-06-19 20:31:44 +0330

    start deploy

--- src/components/shared/page-wrapper.tsx
+++ src/components/shared/page-wrapper.tsx
@@ -8,8 +8,8 @@ interface PageWrapperProps {
 export function PageWrapper({ header, children }: PageWrapperProps) {
   return (
     <div className="flex flex-col h-full mb-6">
-      {header && <div className="shrink-0 p-6 pb-2">{header}</div>}
-      <div className="flex-1 flex flex-col overflow-y-auto space-y-6 p-6 pt-2">
+      {header && <div className="shrink-0 p-4 lg:p-6 pb-2">{header}</div>}
+      <div className="flex-1 flex flex-col overflow-y-auto space-y-6 p-4 lg:p-6 pt-2">
         {children}
       </div>
     </div>
